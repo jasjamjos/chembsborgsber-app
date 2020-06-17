7b48146d681b852a2ef396b69aa4529140366493
jasper <jasjamjos@gmail.com>
2020-06-17 14:48:41 +0800

    removed type

--- src/components/BurgerBuilder/BurgerBuilder.js
+++ src/components/BurgerBuilder/BurgerBuilder.js
@@ -1,4 +1,4 @@
-import React, { useState, useEFfect, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Burger from '../Burger/Burger';
 import BuildControls from '../Burger/BuildControls/BuildControls';
